5de67a8158b8ba4f3caaf1eded01fb61aaecee44
Filip Wachowiak <filipw01@interia.eu>
2021-03-14 11:18:02 +0100

    Revert "fix(DivesLink): use LinkOverlay instead of Link"
    This reverts commit 7a17c30ab77b03c5a2ca93f8d357c50419f2bf5e.

--- src/components/DivesLink.tsx
+++ src/components/DivesLink.tsx
@@ -1,13 +1,13 @@
 import NextLink from 'next/link';
-import { LinkOverlay } from '@chakra-ui/react';
+import { Link } from '@chakra-ui/react';
 import React from 'react';
 
 const DivesLink = ({ children, href }: React.PropsWithChildren<{ href: string }>) => (
   <NextLink href={href} passHref>
     {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-    <LinkOverlay color="dives.green" fontWeight="semibold">
+    <Link color="dives.green" fontWeight="semibold">
       {children}
-    </LinkOverlay>
+    </Link>
   </NextLink>
 );
 
