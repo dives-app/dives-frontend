9f2c1dcb7d7b47b15e51d173c2cecd25b1938783
Filip Wachowiak <filipw01@interia.eu>
2021-03-14 13:24:01 +0100

    feat(ActiveLink): component for chakra style override when link active

---
+++ src/components/ActiveLink.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import NextLink, { LinkProps } from 'next/link';
+import { CSSObject } from '@chakra-ui/styled-system';
+
+type ActiveLinkProps = LinkProps & {
+  activeStyle: CSSObject | undefined;
+};
+
+const ActiveLink = ({
+  children,
+  href,
+  as,
+  passHref,
+  locale,
+  prefetch,
+  replace,
+  scroll,
+  shallow,
+  activeStyle,
+}: React.PropsWithChildren<ActiveLinkProps>) => {
+  const { asPath } = useRouter();
+  const child = React.Children.only(children) as React.ReactElement;
+  const isActive = asPath === href || asPath === as;
+  return (
+    <NextLink
+      href={href}
+      as={as}
+      passHref={passHref}
+      locale={locale}
+      prefetch={prefetch}
+      replace={replace}
+      scroll={scroll}
+      shallow={shallow}
+    >
+      {React.cloneElement(child, isActive ? activeStyle : undefined)}
+    </NextLink>
+  );
+};
+
+export default ActiveLink;
